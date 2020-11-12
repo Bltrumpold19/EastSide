e5aef54f1fee31d1d5568f6db5fe7b958d9e73c0
Brandon Trumpold <bltrumpold@email.neit.edu>
2020-11-12 17:03:04 -0500

    First Commit

---
+++ ESPPWeb/eastSide.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const hbs = require('hbs')
+const AWS = require('aws-sdk')
+
+const app = express()
+app.set('view engine', 'hbs')
+app.use(express.static(__dirname + '/views/images'))
+hbs.registerPartials(__dirname + '/views/partials')
+app.use(express.static(__dirname + '/public'))
+app.use(express.urlencoded({extended:false}))
+
+
+app.get('/', (req,res)=>{
+    res.render('index')
+})
+
+app.get('/index', (req,res)=>{
+    res.render('index')
+})
+
+app.get('/about', (req,res)=>{
+    res.render('about')
+})
+
+app.get('/menu', (req,res)=>{
+
+    const menu = {
+        "appetizers": [
+            {
+                "menuItems" : [
+                    {
+                        "itemName": "Hot Wings"
+                    },
+                    {
+                        "itemName": "Mozz Sticks"
+                    }
+                ]
+            }
+        ]
+    }
+
+    res.render('menu')
+})
+
+app.get('/*', (req,res)=>{
+    res.render('404')
+})
+
+const port = process.env.PORT || 80
+app.listen(port, ()=>{console.log("Server running on port " + port)})
