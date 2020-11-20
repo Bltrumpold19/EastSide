4f1549fe6431366977af52f4dea7c5d40960015a
Bltrumpold19 <bltrumpold@gmail.com>
2020-11-19 19:35:57 -0500

    HTML/CSS & JSON

--- ESPPWeb/eastSide.js
+++ ESPPWeb/eastSide.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const hbs = require('hbs')
 const AWS = require('aws-sdk')
+const { CodeBuild } = require('aws-sdk')
+const fetch = require("node-fetch"); 
+const fs = require('fs-extra')
 
 const app = express()
 app.set('view engine', 'hbs')
@@ -9,6 +12,13 @@ hbs.registerPartials(__dirname + '/views/partials')
 app.use(express.static(__dirname + '/public'))
 app.use(express.urlencoded({extended:false}))
 
+
+hbs.registerHelper("getMenuData", (id,cb)=>{
+    let data = fs.readFileSync(__dirname + '/menu.json')
+    let menu = JSON.parse(data)
+    console.log(menu.appetizers.itemSizesSize[0])
+    //console.log(menu)
+})
 
 app.get('/', (req,res)=>{
     res.render('index')
