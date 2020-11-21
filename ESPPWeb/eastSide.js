27e1fbe1abc6f2f7f8ebe5ab6cf466b4333a48c1
Bltrumpold19 <bltrumpold@gmail.com>
2020-11-21 15:34:23 -0500

    MENU page

--- ESPPWeb/eastSide.js
+++ ESPPWeb/eastSide.js
@@ -13,21 +13,50 @@ app.use(express.static(__dirname + '/public'))
 app.use(express.urlencoded({extended:false}))
 
 
-hbs.registerHelper("getMenuData", (id,cb)=>{
+hbs.registerHelper("getMenuDataPizza", (id,cb)=>{
 
     var str = ''
 
     let data = fs.readFileSync(__dirname + '/menu.json')
     let menu = JSON.parse(data)
 
+    str += '<div id="pizzaCont">'
+    str += '<div id="pizzaContHead">'
+    str += '</div>' //  end pizzaContHead
+    str += '<div id="pizzaTopCont">'
     for (var i = 0; i < menu.pizza.pizzaToppings.length; i++)
     {
-        console.log(menu.pizza.pizzaToppings[i])
+        str += menu.pizza.pizzaToppings[i]
     }
-
-    //console.log(menu.appetizers.itemSizesSize[0])
-    //console.log(menu)
+    str += '</div>' // end pizzaTopCont
+    str += '<div id="sizesCont">'
+    for (var i = 0; i < menu.pizza.sizes.length; i++)
+    {
+        str += '<li class="pizzaSize">' + menu.pizza.sizes[i] + '</li>'
+    }
+    str += '</div>' // end sizesCont
+    str += '<div id="toppingAmountCont">'
+    for (var i = 0; i < menu.pizza.toppingAmounts.length; i++)
+    {
+        str += '<li class="toppingAmount">' + menu.pizza.toppingAmounts[i] + '</li>'
+    }
+    str += '</div>' // end toppingAmountCont
+    str += '<div id="pizzaPriceCont">'
+    for (var i = 0; i < menu.pizza.prices.small.length; i++)
+    {
+        str += '<li class="smallPrices">' + menu.pizza.prices.small[i] + '</li>'
+        str += '<li class="mediumPrices">' + menu.pizza.prices.medium[i] + '</li>'
+        str += '<li class="largePrices">' + menu.pizza.prices.large[i] + '</li>'
+    }
+    for(var i = 0; i < menu.pizza.prices.specialTop.length; i++)
+    {
+        str += '<li class="specialPrices">' + menu.pizza.prices.specialTop[i] + '</li>'
+    }
+    str += '</div>' // end pizzaPriceCont
+    str += '</div>' // end pizzaCont
+    return new hbs.handlebars.SafeString(str)
 })
+
 
 app.get('/', (req,res)=>{
     res.render('index')
